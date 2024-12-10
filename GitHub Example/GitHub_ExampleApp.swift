35f3e711adae144878648523916c84f6b4fc65e4
Lawrence Whitlow <whitlowl95@outlook.com>
2024-12-10 09:40:45 -0500

    Initial Commit

---
+++ GitHub Example/GitHub_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExampleApp.swift
+//  GitHub Example
+//
+//  Created by Lawrence Whitlow on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitHub ExampleTests/GitHub_ExampleTests.swift
@@ -0,0 +1,16 @@
+//
+//  GitHub_ExampleTests.swift
+//  GitHub ExampleTests
+//
+//  Created by Lawrence Whitlow on 12/10/24.
+//
+
+import Testing
+
+struct GitHub_ExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
